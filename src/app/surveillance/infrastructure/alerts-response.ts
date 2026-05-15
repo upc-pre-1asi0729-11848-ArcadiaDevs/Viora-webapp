9c58288062b7618f6ea4a25f4464a3d8f499ca65
Jahat Trininidad <jahatjassiel@gmail.com>
2026-05-15 10:38:53 -0500

    feat(alert): add api resource payload definition for alerts data..

---
+++ src/app/surveillance/infrastructure/alerts-response.ts
@@ -0,0 +1,30 @@
+/**
+ * @file alerts-response.ts
+ * @description API resource payload definition for Alerts data.
+ */
+import {
+  BaseResource,
+  BaseResponse,
+  CollectionResponse,
+} from '../../shared/infrastructure/base-response';
+
+export interface AlertPlotResource {
+  name?: string;
+  location?: string;
+  hectares?: number;
+}
+
+export interface AlertResource extends BaseResource {
+  type?: string;
+  description?: string;
+  severity?: string;
+  date?: string;
+  status?: string;
+  plot?: AlertPlotResource;
+}
+
+export interface AlertsResponse extends BaseResponse {
+  alerts: AlertResource[];
+}
+
+export type AlertCollectionResponse = CollectionResponse<AlertResource, 'alerts'>;
